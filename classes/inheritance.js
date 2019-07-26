e0620ec5d4627b0e1eeb89bdc973144a287794ce
David Jones <david@dcj.codes>
2019-07-26 13:25:34 -0400

    JS : Class inheritance expansion

--- classes/inheritance.js
+++ classes/inheritance.js
@@ -1,3 +1,4 @@
+// EXAMPLE 1 - Simple inheritance
 // Define Super Class
 class Human {
   speak(person) {
@@ -26,3 +27,40 @@ const David = new Person();
 
 // Run Person method
 David.action("David");
+
+
+// EXAMPLE 2 - Further Inheritance
+
+// Define generic class
+class Car {
+  constructor({ make, model}) {
+    this.make = make;
+    this.model = model
+  }
+
+  drive() {
+    return `The ${this.make} ${this.model} is a driving`;
+  }
+  
+}
+
+// Define subclass
+// this will inherit from Car generic
+class Porsche extends Car {
+  constructor({ make, model, color }) {
+    super({ make, model }); // Call parent constructor
+    this.color = color;
+  }
+
+}
+
+const _911 = new Porsche({ make: "Porsche", model: 911, color: "White"});
+
+// The following will contain the make, model, and color
+// the make and model is a result of super calling the generic constructor
+// and passing down it's initiazation properties to the subclass
+// and the color comes from the subclass constructor
+console.log(_911); // { make: 'Porsche', model: 911, color: 'White' }
+
+// We can also call the parent class' methods
+console.log(_911.drive()); // The Porsche 911 is a driving
