4e36dae8e494cd4b68a36528b7253db28584dcb1
David Jones <david@dcj.codes>
2019-08-12 14:07:42 -0400

    JS : Scope - Closures; add code snippets

--- src/scope/closure/closures.js
+++ src/scope/closure/closures.js
@@ -8,4 +8,53 @@ function outer(thing) {
 
 const inner = outer('I AM INSIDE');
 
-inner(); // I AM INSIDE OUTER
+inner(); //{} I AM INSIDE OUTER
+
+//* EXAMPLE 2 - Basic Privacy and currying
+function AnAPI(a) {
+
+  function adder(a, b) {
+    return `The result is: ${a + b}`;
+  }
+
+  return function result(b) {
+    return  adder(a, b); //{} the a binding is closed over by the AnAPI function.
+  }
+
+}
+
+const plusOne = AnAPI(1);
+
+plusOne(2); //{} "The result is: 3"
+
+
+//* EXAMPLE 3 - More complex privacy and exposure (taken from: https://www.freecodecamp.org/news/learn-javascript-closures-in-n-minutes/ )
+const BankAccountAPI = function(initialBalance) {
+  let balance = initialBalance; //! private, closed-over binding, only accessible within this lexical environment
+  
+  //* Return public API methods
+  //* the accountBalance is what is closed over
+  return {
+      getBalance: function() { 
+        return `${balance}`; 
+      },
+      deposit: function(amount) { 
+        balance += amount; 
+      },
+      withdraw: function(amount) {
+          if (amount > balance) {
+              return 'Not enough dollar!';
+          }
+
+          balance -= amount;
+      }
+  };
+}
+
+//* Set initial amount
+const  BankManager = BankAccountAPI(0);
+
+//* Deposit funds
+BankManager.getBalance(); //{} 0
+BankManager.deposit(1000);
+BankManager.getBalance(); //{} 1000
