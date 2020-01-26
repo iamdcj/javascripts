8f6c3368e9d2cba67ca8571572df13bd717ae8cf
David Christian Jones <david@dcj.codes>
2020-01-26 15:59:26 -0500

    find clear path

---
+++ problems/find-clear-path.js
@@ -0,0 +1,40 @@
+const solution = pot => {
+  const check = pot.map((row, i) => {
+    nextRow = pot[i + 1];
+
+    if (!nextRow) {
+      return;
+    }
+
+    return checkForPath(row, nextRow);
+  });
+
+  return !check.includes(false);
+};
+
+const returnIndexes = arr =>
+  arr
+    .map((c, i) => {
+      return c === 0 ? i : null;
+    })
+    .filter(c => c !== null);
+
+function checkForPath(row, nextRow) {
+  const currentIndexes = returnIndexes(row);
+  const nextIndexes = returnIndexes(nextRow);
+
+  const intersection = currentIndexes.some(value => {
+    return nextIndexes.includes(value);
+  });
+
+  return intersection;
+}
+
+console.log(
+  solution([
+    [1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1],
+    [0, 1, 1, 1, 1],
+    [0, 1, 1, 1, 1]
+  ])
+);
