8825d8040e163549a8556ceb7517c075b123e923
David Christian Jones <david@dcj.codes>
2020-03-03 17:57:33 -0500

    promise race

---
+++ core/asynchronicity/promises/_examples/race.js
@@ -0,0 +1,33 @@
+const promiser = (id, delay) => {
+  return new Promise(resolve => {
+    setTimeout(resolve, delay, id);
+  });
+};
+
+// timeout
+function timeout(timeout, promise) {
+  let timer;
+
+  const timeoutPromise = new Promise((_, reject) => {
+    timer = setTimeout(() => {
+      reject(Error("TIMED OUT"));
+    }, timeout);
+  });
+
+  return Promise.race([promise, timeoutPromise]).finally(() => {
+    clearTimeout(timer);
+    console.log("CLEARED");
+  });
+}
+
+timeout(1250, promiser("a", 3000))
+  .then(value => console.log(value))
+  .catch(e => console.log(e.message));
+
+// TIMED OUT
+
+timeout(250, promiser("a", 100))
+  .then(value => console.log(value))
+  .catch(e => console.log(e.message));
+
+// a
